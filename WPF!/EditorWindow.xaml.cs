e9cd6065dda74084a4a2729a7e0482e062a0f74b
snovis <sergeygoodgood@gmail.com>
2020-08-02 21:03:41 +0300

    f

---
+++ WPF!/EditorWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_
+{
+    /// <summary>
+    /// Interaction logic for EditorWindow.xaml
+    /// </summary>
+    public partial class EditorWindow : Window
+    {
+        public EditorWindow(string text)
+        {
+            InitializeComponent();
+            this.editonText.Text = text;
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        public string GetContent()
+        {
+            return this.editonText.Text;
+        }
+    }
+}
--- WPF!/MainWindow.xaml.cs
+++ WPF!/MainWindow.xaml.cs
@@ -33,10 +33,22 @@ namespace WPF_
             this.GoalList.ItemsSource = Tasks;
         }
 
+        private string GetTaskText(string baseContent)
+        {
+            EditorWindow editor = new EditorWindow(baseContent);
+            editor.Owner = this;
+            if (editor.ShowDialog() == true)
+            {
+                return editor.editonText.Text;
+            }
+            return baseContent;
+        }
         public void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            Tasks.Add(new Task { Content = $"{count++}" });
+            var content = GetTaskText("");
+            Tasks.Add(new Task { Content = $"{content}" });
         }
+
         public void ButtonEraser_Click(object sender, RoutedEventArgs e)
         {
             var Button_sender = (Button)sender;
@@ -56,8 +68,11 @@ namespace WPF_
             var Border_sender = (Border)ContentPresenter_sender.Parent;
             var ListViewItem_sender = (ListViewItem)(Border_sender.TemplatedParent);
             var WPF_Task_sender = (WPF_.Task)ListViewItem_sender.DataContext;
+            //EditorControl editorControl = new EditorControl(WPF_Task_sender.Content);
+            //MainGrid.Children.Add(editorControl);
 
             int index = Tasks.IndexOf(WPF_Task_sender);
+            Tasks[index] = new Task { Content = $"{GetTaskText(WPF_Task_sender.Content)}" };
         }
     }
 }
